c21d6b56727bf7eb2934def7e62bcf2879f5261b
Shurekov Sergei <gsn-78@mail.ru>
2024-03-02 14:37:23 +0300

    Добавлена программа по заданию 3.3.2

---
+++ Task3.3.2/Task3.3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static void Main(string[] args)
+    {
+        string Name = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
+
+        Console.WriteLine("My name is " + Name);
+        Console.WriteLine("My Age " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+
+
+
+
+    }
+
+
+}
+
